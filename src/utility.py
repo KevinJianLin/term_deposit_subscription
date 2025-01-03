1b5b354de189618e5bcd6b76c953171446e50201
Jian Bin lin <jianbinlin@Jians-MacBook-Air.local>
2025-01-02 21:29:04 -0500

    ii

--- src/utility.py
+++ src/utility.py
@@ -19,6 +19,16 @@ from sklearn.pipeline import Pipeline
 
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score  # Ensure recall_score is imported
+from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import log_loss
+from sklearn.metrics import matthews_corrcoef
+from sklearn.metrics import cohen_kappa_score
+from sklearn.metrics import balanced_accuracy_score
+
 
 
 class data_profiling:
